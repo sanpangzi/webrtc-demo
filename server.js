ee210b5bbb334590c2ac718e7f529687e1799537
qhz <243127387@qq.com>
2018-07-20 09:57:53 +0800

    listen index.html
    listen index.html success, I install express.

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-var http = require('http');
-http.createServer(function (request, response) {
 
-    // 发送 HTTP 头部 
-    // HTTP 状态值: 200 : OK
-    // 内容类型: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+var app =require('express')();
+var http = require('http').Server(app);
 
-    // 发送响应数据 "Hello World"
-    response.end('Hello World\n');
-}).listen(8888);
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
-// 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8888/');
+http.listen(8888,function(){
+	console.log('Server running at http://127.0.0.1:8888/');
+});
