01a86a74eb3971925ebc7ff7855658f25930aa2d
qhz <243127387@qq.com>
2018-07-25 14:53:58 +0800

    add demo measure volume
    add demo
    9Audio_only_getUserMedia_displaying_volume.html

---
+++ static/js/main6.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Put variables in global scope to make them available to the browser console.
+const audio = document.querySelector('audio');
+
+const constraints = window.constraints = {
+  audio: true,
+  video: false
+};
+
+function handleSuccess(stream) {
+  const audioTracks = stream.getAudioTracks();
+  console.log('Got stream with constraints:', constraints);
+  console.log('Using audio device: ' + audioTracks[0].label);
+  stream.oninactive = function() {
+    console.log('Stream ended');
+  };
+  window.stream = stream; // make variable available to browser console
+  audio.srcObject = stream;
+}
+
+function handleError(error) {
+  console.log('navigator.getUserMedia error: ', error);
+}
+
+navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
